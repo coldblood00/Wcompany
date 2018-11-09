21a65d997db5dd237d8a507a20e4a8c1cd251cc5
Mr.K <krushilnarola15@gmail.com>
2018-11-09 20:36:07 +0530

    Fourth

--- Wcompany/settings.py
+++ Wcompany/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'wdevnet/static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'Wcompany/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
